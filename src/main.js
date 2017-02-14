efe2d7da202b1cd4993dd5da8df1cbe849ed383e
tkhm <no-reply@tkhm.dev>
2017-02-14 22:02:11 +0900

    Add all battle record on table

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ let d3 = require('d3');
 d3.json('/data/records.json', (data) => {
   d3.json('/data/people.json', (people) => {
     let peopleArray = {};
+    let tableArray = [];
 
     // 積み上げ成績を出すための配列の作成
     for(let i = 0; i < people.length; i++) {
@@ -20,6 +21,10 @@ d3.json('/data/records.json', (data) => {
 
     for(let i = 0; i < data.length; i++) {
       let recordedDate = data[i].date;
+      let recordedNotes = data[i].notes;
+      let tableContent = {};
+      tableContent['date'] = recordedDate;
+      tableContent['notes'] = recordedNotes;
 
       for(let j = 0; j < data[i].record.length; j++) {
         let eachRecord = data[i].record[j];
@@ -27,17 +32,15 @@ d3.json('/data/records.json', (data) => {
           'date': recordedDate,
           'score': eachRecord.score
         });
+        tableContent[eachRecord.name] = eachRecord.score;
       }
+      tableArray.push(tableContent);
     }
-
-    console.dir(peopleArray);
 
     for(let k = 0; k < people.length; k++) {
       let lastValue = 0;
       let records = peopleArray[people[k]];
 
-      console.log(people[k]);
-      console.dir(peopleArray[people[k]]);
       for(let l = 0; l < records.length; l++) {
         records[l].score = records[l].score + lastValue;
         lastValue = records[l].score;
@@ -52,8 +55,6 @@ d3.json('/data/records.json', (data) => {
       });
       peopleArray[people[k]] = records;
     }
-
-    console.dir(peopleArray);
 
     let dataArray = [];
     for(let m = 0; m < people.length; m++) {
@@ -73,6 +74,36 @@ d3.json('/data/records.json', (data) => {
       y_accessor: 'score', // the key that accesses the y value
       y_label: '累積スコア'
     });
+
+    let dataArray2 = [];
+    // 一人ごとの配列, dateとscoreが入っているオブジェクト
+    for (let arr of dataArray) {
+      for (let n = 0; n < arr.length; n++) {
+        dataArray2.push({date: arr.date, record: []});
+      }
+    }
+
+    MG.data_table({
+        title: "戦歴",
+        description: "A table is often the most appropriate way to present data. We aim to make the creation of data tables very simple. We are working on implementing sparklines, bullet charts, and other niceties.",
+        data: tableArray,
+        show_tooltips: true
+    })
+    .target('#table')
+    .title({accessor: 'date', label: 'date'})
+    .number({accessor: 'Mot', label: 'Mot'})
+    .number({accessor: 'Jin', label: 'Jin'})
+    .number({accessor: 'Koj', label: 'Koj'})
+    .number({accessor: 'End', label: 'End'})
+    .number({accessor: 'Shk', label: 'Shk'})
+    .number({accessor: 'Oka', label: 'Oka'})
+    .number({accessor: 'Yam', label: 'Yam'})
+    .number({accessor: 'Kos', label: 'Kos'})
+    .number({accessor: 'Ogu', label: 'Ogu'})
+    .text({accessor: 'notes', label: 'Notes'})
+    .display();
+
+    console.dir(dataArray);
   });
 });
 
